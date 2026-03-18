cea6a7795fcc9547c61858fb0621ee7ed2133448
Shardul Dhiwar <sujaldhiwar007@gmail.com>
2026-03-18 17:00:46 +0530

    added  the render live link

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,7 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000", {
+const socket = io("https://multiplayer-drawing-game-zytf.onrender.com", {
   autoConnect: false,
 });
 
