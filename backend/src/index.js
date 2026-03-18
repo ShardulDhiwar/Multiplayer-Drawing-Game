dd6c38b6037ced5e902713287b95f2944732da73
Shardul Dhiwar <sujaldhiwar007@gmail.com>
2026-03-18 16:56:28 +0530

    git changes in CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Vite default port
+    origin: "*", // Vite default port
     methods: ["GET", "POST"],
   },
   // Reconnection support
